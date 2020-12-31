b0382924a778db774670ac09a707a2c00f7ae61e
Daniel Naaman <danielnaaman@icloud.com>
2020-12-31 18:38:02 +0200

    Make randomized appearences and add help buttons

--- Simon/Form1.Designer.cs
+++ Simon/Form1.Designer.cs
@@ -29,33 +29,40 @@ namespace Simon
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.pictureBox_Red = new System.Windows.Forms.PictureBox();
             this.pictureBox_Green = new System.Windows.Forms.PictureBox();
             this.pictureBox_Yellow = new System.Windows.Forms.PictureBox();
             this.pictureBox_Blue = new System.Windows.Forms.PictureBox();
             this.button_Start = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button_Instructions = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Red)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Green)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Yellow)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Blue)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox_Red
             // 
             this.pictureBox_Red.BackColor = System.Drawing.Color.Red;
-            this.pictureBox_Red.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox_Red.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox_Red.Location = new System.Drawing.Point(15, 19);
             this.pictureBox_Red.Name = "pictureBox_Red";
-            this.pictureBox_Red.Size = new System.Drawing.Size(128, 128);
+            this.pictureBox_Red.Size = new System.Drawing.Size(110, 110);
             this.pictureBox_Red.TabIndex = 0;
             this.pictureBox_Red.TabStop = false;
             this.pictureBox_Red.Click += new System.EventHandler(this.pictureBox_Click);
             // 
             // pictureBox_Green
             // 
-            this.pictureBox_Green.BackColor = System.Drawing.Color.Lime;
-            this.pictureBox_Green.Location = new System.Drawing.Point(146, 12);
+            this.pictureBox_Green.BackColor = System.Drawing.Color.Green;
+            this.pictureBox_Green.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox_Green.Location = new System.Drawing.Point(136, 19);
             this.pictureBox_Green.Name = "pictureBox_Green";
-            this.pictureBox_Green.Size = new System.Drawing.Size(128, 128);
+            this.pictureBox_Green.Size = new System.Drawing.Size(110, 110);
             this.pictureBox_Green.TabIndex = 1;
             this.pictureBox_Green.TabStop = false;
             this.pictureBox_Green.Click += new System.EventHandler(this.pictureBox_Click);
@@ -63,9 +70,10 @@ namespace Simon
             // pictureBox_Yellow
             // 
             this.pictureBox_Yellow.BackColor = System.Drawing.Color.Yellow;
-            this.pictureBox_Yellow.Location = new System.Drawing.Point(146, 146);
+            this.pictureBox_Yellow.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox_Yellow.Location = new System.Drawing.Point(137, 140);
             this.pictureBox_Yellow.Name = "pictureBox_Yellow";
-            this.pictureBox_Yellow.Size = new System.Drawing.Size(128, 128);
+            this.pictureBox_Yellow.Size = new System.Drawing.Size(110, 110);
             this.pictureBox_Yellow.TabIndex = 2;
             this.pictureBox_Yellow.TabStop = false;
             this.pictureBox_Yellow.Click += new System.EventHandler(this.pictureBox_Click);
@@ -73,39 +81,75 @@ namespace Simon
             // pictureBox_Blue
             // 
             this.pictureBox_Blue.BackColor = System.Drawing.Color.Blue;
-            this.pictureBox_Blue.Location = new System.Drawing.Point(12, 146);
+            this.pictureBox_Blue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox_Blue.Location = new System.Drawing.Point(15, 140);
             this.pictureBox_Blue.Name = "pictureBox_Blue";
-            this.pictureBox_Blue.Size = new System.Drawing.Size(128, 128);
+            this.pictureBox_Blue.Size = new System.Drawing.Size(110, 110);
             this.pictureBox_Blue.TabIndex = 3;
             this.pictureBox_Blue.TabStop = false;
             this.pictureBox_Blue.Click += new System.EventHandler(this.pictureBox_Click);
             // 
             // button_Start
             // 
-            this.button_Start.Location = new System.Drawing.Point(13, 281);
+            this.button_Start.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_Start.Location = new System.Drawing.Point(12, 281);
             this.button_Start.Name = "button_Start";
-            this.button_Start.Size = new System.Drawing.Size(261, 23);
+            this.button_Start.Size = new System.Drawing.Size(128, 28);
             this.button_Start.TabIndex = 4;
             this.button_Start.Text = "Start";
             this.button_Start.UseVisualStyleBackColor = true;
             this.button_Start.Click += new System.EventHandler(this.button_Start_Click);
             // 
+            // timer1
+            // 
+            this.timer1.Interval = 400;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox_Red);
+            this.groupBox1.Controls.Add(this.pictureBox_Green);
+            this.groupBox1.Controls.Add(this.pictureBox_Yellow);
+            this.groupBox1.Controls.Add(this.pictureBox_Blue);
+            this.groupBox1.Location = new System.Drawing.Point(12, 9);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(261, 262);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Colors";
+            // 
+            // button_Instructions
+            // 
+            this.button_Instructions.Cursor = System.Windows.Forms.Cursors.Help;
+            this.button_Instructions.Location = new System.Drawing.Point(143, 281);
+            this.button_Instructions.Name = "button_Instructions";
+            this.button_Instructions.Size = new System.Drawing.Size(128, 28);
+            this.button_Instructions.TabIndex = 6;
+            this.button_Instructions.Text = "Instructions";
+            this.button_Instructions.UseVisualStyleBackColor = true;
+            this.button_Instructions.Click += new System.EventHandler(this.button_Instructions_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(285, 314);
+            this.ClientSize = new System.Drawing.Size(285, 321);
+            this.Controls.Add(this.button_Instructions);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button_Start);
-            this.Controls.Add(this.pictureBox_Blue);
-            this.Controls.Add(this.pictureBox_Yellow);
-            this.Controls.Add(this.pictureBox_Green);
-            this.Controls.Add(this.pictureBox_Red);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.HelpButton = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
+            this.ShowIcon = false;
             this.Text = "Simon by Daniel Naaman and Yonatan Shoshani";
+            this.HelpButtonClicked += new System.ComponentModel.CancelEventHandler(this.Form1_HelpButtonClicked);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Red)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Green)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Yellow)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Blue)).EndInit();
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -117,6 +161,9 @@ namespace Simon
         private System.Windows.Forms.PictureBox pictureBox_Yellow;
         private System.Windows.Forms.PictureBox pictureBox_Blue;
         private System.Windows.Forms.Button button_Start;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button_Instructions;
     }
 }
 
--- Simon/Form1.cs
+++ Simon/Form1.cs
@@ -13,64 +13,118 @@ namespace Simon
 {
     public partial class Form1 : Form
     {
+        Random rnd = new Random();
+
+        bool isClicked = false;
+        int[] colorsArray = new int[10];
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        // Red    - 1
-        // Green  - 2
-        // Blue   - 3
-        // Yellow - 4
-
         public void button_Start_Click(object sender, EventArgs e)
         {
-            int[] array = new int[10];
-            Random rnd = new Random();
-            int num = 1;
-
-            for (int i = 0; i < 10; i++)
+            if (!isClicked)
             {
-                array[i] = rnd.Next(1, 5);
+                timer1.Start();
+                isClicked = true;
+                button_Start.Text = "Stop / Reset";
             }
 
-            Thread.Sleep(3000);
-
-            for (int i = 0; i < array.Length; i++)
+            else
             {
-                while (num <= i)
-                {
-                    if (array[num] == 1)
-                    {
-                        pictureBox_Red.BackColor = Color.DarkRed;
-                        num++;
-                    }
-                    
-                    else if (array[num] == 2)
-                    {
-                        pictureBox_Green.BackColor = Color.DarkGreen;
-                        num++;
-                    }
+                timer1.Stop();
+                isClicked = false;
+                button_Start.Text = "Start";
 
-                    else if (array[num] == 3)
-                    {
-                        pictureBox_Blue.BackColor = Color.DarkBlue;
-                        num++;
-                    }
-
-                    else if (array[num] == 4)
-                    {
-                        pictureBox_Yellow.BackColor = Color.DarkOrange;
-                        num++;
-                    }
-                    Thread.Sleep(3000);
-                }
+                pictureBox_Red.BackColor = Color.Red;
+                pictureBox_Green.BackColor = Color.Green;
+                pictureBox_Blue.BackColor = Color.Blue;
+                pictureBox_Yellow.BackColor = Color.Yellow;
             }
         }
 
         private void pictureBox_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Red    - 1
+            // Green  - 2
+            // Blue   - 3
+            // Yellow - 4
+
+            for (int i = 0; i < colorsArray.Length; i++)
+            {
+                colorsArray[i] = rnd.Next(1, 9);
+
+                if (i == 1)
+                {
+                    colorsArray[i + 1] = 2;
+                }
+
+                else if (i == 2)
+                {
+                    colorsArray[i + 2] = 3;
+                }
+
+                else if (i == 3)
+                {
+                    colorsArray[i + 3] = 4;
+                }
+
+                else if (i == 4)
+                {
+                    colorsArray[i + 4] = 5;
+                }
+
+                i++;
+            }
+
+            for (int i = 0; i < colorsArray.Length; i++)
+            {
+                switch (colorsArray[i])
+                {
+                    case 1:
+                        pictureBox_Red.BackColor = Color.Salmon;
+                        break;
+                    case 2:
+                        pictureBox_Red.BackColor = Color.Red;
+                        break;
+                    case 3:
+                        pictureBox_Green.BackColor = Color.LightGreen;
+                        break;
+                    case 4:
+                        pictureBox_Green.BackColor = Color.Green;
+                        break;
+                    case 5:
+                        pictureBox_Blue.BackColor = Color.LightBlue;
+                        break;
+                    case 6:
+                        pictureBox_Blue.BackColor = Color.Blue;
+                        break;
+                    case 7:
+                        pictureBox_Yellow.BackColor = Color.LightYellow;
+                        break;
+                    case 8:
+                        pictureBox_Yellow.BackColor = Color.Yellow;
+                        break;
+                }
+            }
+        }
+
+        private void button_Instructions_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Show();
+        }
+        private void Form1_HelpButtonClicked(Object sender, CancelEventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Show();
         }
     }
 }
