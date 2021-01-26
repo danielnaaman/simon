1e81a3bf304c28cd2abb7c6c956ca21ecdb17115
Daniel Naaman <danielnaaman@icloud.com>
2021-01-26 20:23:57 +0200

    Add win and loose messages and add level counter

--- Simon/Form1.Designer.cs
+++ Simon/Form1.Designer.cs
@@ -36,60 +36,62 @@ namespace Simon
             this.pictureBox_Blue = new System.Windows.Forms.PictureBox();
             this.button_Start = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button_Instructions = new System.Windows.Forms.Button();
             this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Red)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Green)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Yellow)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Blue)).BeginInit();
-            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox_Red
             // 
-            this.pictureBox_Red.BackColor = System.Drawing.Color.Red;
+            this.pictureBox_Red.BackColor = System.Drawing.Color.OrangeRed;
             this.pictureBox_Red.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBox_Red.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.pictureBox_Red.Location = new System.Drawing.Point(15, 19);
+            this.pictureBox_Red.Location = new System.Drawing.Point(12, 12);
             this.pictureBox_Red.Name = "pictureBox_Red";
-            this.pictureBox_Red.Size = new System.Drawing.Size(110, 110);
+            this.pictureBox_Red.Size = new System.Drawing.Size(123, 123);
+            this.pictureBox_Red.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox_Red.TabIndex = 0;
             this.pictureBox_Red.TabStop = false;
             this.pictureBox_Red.Click += new System.EventHandler(this.pictureBox_Click);
             // 
             // pictureBox_Green
             // 
-            this.pictureBox_Green.BackColor = System.Drawing.Color.Green;
+            this.pictureBox_Green.BackColor = System.Drawing.Color.YellowGreen;
             this.pictureBox_Green.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBox_Green.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.pictureBox_Green.Location = new System.Drawing.Point(136, 19);
+            this.pictureBox_Green.Location = new System.Drawing.Point(142, 12);
             this.pictureBox_Green.Name = "pictureBox_Green";
-            this.pictureBox_Green.Size = new System.Drawing.Size(110, 110);
+            this.pictureBox_Green.Size = new System.Drawing.Size(123, 123);
+            this.pictureBox_Green.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox_Green.TabIndex = 1;
             this.pictureBox_Green.TabStop = false;
             this.pictureBox_Green.Click += new System.EventHandler(this.pictureBox_Click);
             // 
             // pictureBox_Yellow
             // 
-            this.pictureBox_Yellow.BackColor = System.Drawing.Color.Yellow;
+            this.pictureBox_Yellow.BackColor = System.Drawing.Color.Orange;
             this.pictureBox_Yellow.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBox_Yellow.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.pictureBox_Yellow.Location = new System.Drawing.Point(137, 140);
+            this.pictureBox_Yellow.Location = new System.Drawing.Point(142, 144);
             this.pictureBox_Yellow.Name = "pictureBox_Yellow";
-            this.pictureBox_Yellow.Size = new System.Drawing.Size(110, 110);
+            this.pictureBox_Yellow.Size = new System.Drawing.Size(123, 123);
+            this.pictureBox_Yellow.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox_Yellow.TabIndex = 2;
             this.pictureBox_Yellow.TabStop = false;
             this.pictureBox_Yellow.Click += new System.EventHandler(this.pictureBox_Click);
             // 
             // pictureBox_Blue
             // 
-            this.pictureBox_Blue.BackColor = System.Drawing.Color.Blue;
+            this.pictureBox_Blue.BackColor = System.Drawing.Color.DodgerBlue;
             this.pictureBox_Blue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBox_Blue.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.pictureBox_Blue.Location = new System.Drawing.Point(15, 140);
+            this.pictureBox_Blue.Location = new System.Drawing.Point(12, 144);
             this.pictureBox_Blue.Name = "pictureBox_Blue";
-            this.pictureBox_Blue.Size = new System.Drawing.Size(110, 110);
+            this.pictureBox_Blue.Size = new System.Drawing.Size(123, 123);
+            this.pictureBox_Blue.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox_Blue.TabIndex = 3;
             this.pictureBox_Blue.TabStop = false;
             this.pictureBox_Blue.Click += new System.EventHandler(this.pictureBox_Click);
@@ -97,9 +99,9 @@ namespace Simon
             // button_Start
             // 
             this.button_Start.Cursor = System.Windows.Forms.Cursors.Default;
-            this.button_Start.Location = new System.Drawing.Point(12, 281);
+            this.button_Start.Location = new System.Drawing.Point(12, 277);
             this.button_Start.Name = "button_Start";
-            this.button_Start.Size = new System.Drawing.Size(128, 28);
+            this.button_Start.Size = new System.Drawing.Size(253, 28);
             this.button_Start.TabIndex = 4;
             this.button_Start.Text = "Start";
             this.button_Start.UseVisualStyleBackColor = true;
@@ -110,42 +112,26 @@ namespace Simon
             this.timer1.Interval = 500;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.pictureBox_Red);
-            this.groupBox1.Controls.Add(this.pictureBox_Green);
-            this.groupBox1.Controls.Add(this.pictureBox_Yellow);
-            this.groupBox1.Controls.Add(this.pictureBox_Blue);
-            this.groupBox1.Location = new System.Drawing.Point(12, 9);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(261, 262);
-            this.groupBox1.TabIndex = 5;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Colors";
-            // 
-            // button_Instructions
-            // 
-            this.button_Instructions.Cursor = System.Windows.Forms.Cursors.Help;
-            this.button_Instructions.Location = new System.Drawing.Point(143, 281);
-            this.button_Instructions.Name = "button_Instructions";
-            this.button_Instructions.Size = new System.Drawing.Size(128, 28);
-            this.button_Instructions.TabIndex = 6;
-            this.button_Instructions.Text = "Instructions";
-            this.button_Instructions.UseVisualStyleBackColor = true;
-            this.button_Instructions.Click += new System.EventHandler(this.button_Instructions_Click);
-            // 
             // timer2
             // 
             this.timer2.Interval = 500;
             this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
             // 
+            // timer3
+            // 
+            this.timer3.Enabled = true;
+            this.timer3.Interval = 2000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(285, 321);
-            this.Controls.Add(this.button_Instructions);
-            this.Controls.Add(this.groupBox1);
+            this.ClientSize = new System.Drawing.Size(277, 314);
+            this.Controls.Add(this.pictureBox_Red);
+            this.Controls.Add(this.pictureBox_Green);
+            this.Controls.Add(this.pictureBox_Yellow);
+            this.Controls.Add(this.pictureBox_Blue);
             this.Controls.Add(this.button_Start);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.HelpButton = true;
@@ -159,7 +145,6 @@ namespace Simon
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Green)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Yellow)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Blue)).EndInit();
-            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -172,9 +157,8 @@ namespace Simon
         private System.Windows.Forms.PictureBox pictureBox_Blue;
         private System.Windows.Forms.Button button_Start;
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button button_Instructions;
         private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
     }
 }
 
--- Simon/Form1.cs
+++ Simon/Form1.cs
@@ -13,32 +13,55 @@ namespace Simon
 {
     public partial class Form1 : Form
     {
-        private int[] colors = new int[3];
-        private int blink;
-        private int placeInArray = 0;
-        private int runtime = 0;
-        private int click = 0;
+        private int[] colors = new int[10]; // Random values (1-4) that reflects the sequence of blinks
+        private int placeInArray = 0; // The current place in the colors array (colors[placeInArray])
+        private int blink; // The current color that should blink
+        private int runtime = 0; // Amount of clicks total for this runtime
+        private int click = 1; // 
+        private int countClick = 1; // 
+        private int currentLevel = 1;
+        private bool currentLevelStatus = false;
         private PictureBox pictureBox;
 
         public Form1()
         {
+            // Constructor: Initialize the form and trigger the "FillRandoms()" function
             InitializeComponent();
             FillRandoms();
         }
 
-        void FillRandoms()
+        private void FillRandoms()
         {
             Random rnd = new Random();
 
-            //for (int i = 0; i < colors.Length; i++)
-            //{
-            //    colors[i] = rnd.Next(1, 5); // 1 - 4
-            //}
+            for (int i = 0; i < colors.Length; i++)
+            {
+                colors[i] = rnd.Next(1, 5); // 1 - 4
+            }
 
-            colors[0] = 1;
-            colors[1] = 2;
-            colors[2] = 3;
             blink = colors[0];
+        }
+        
+        private void CheckWinner(bool isWinner)
+        {
+            // Checks if amount of total clicks is equal to size of colors array
+            if (isWinner && countClick == colors.Length)
+            {
+                pictureBox_Red.Image = Simon.Properties.Resources.You;
+                pictureBox_Green.Image = Simon.Properties.Resources.Have;
+                pictureBox_Blue.Image = Simon.Properties.Resources.Gray;
+                pictureBox_Yellow.Image = Simon.Properties.Resources.Hair;
+                MessageBox.Show("Oh, and you've won! Ho.. Ho.. Ho..");
+            }
+
+            else if (countClick < runtime)
+            {
+                pictureBox_Red.Image = Simon.Properties.Resources.You;
+                pictureBox_Green.Image = Simon.Properties.Resources.Have;
+                pictureBox_Blue.Image = Simon.Properties.Resources.Gray;
+                pictureBox_Yellow.Image = Simon.Properties.Resources.Hair;
+                MessageBox.Show("Oh, and you've lost! You are worthless. Shimon is god. Praise shimon.");
+            }
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -49,53 +72,134 @@ namespace Simon
                 placeInArray = 0;
                 blink = colors[0];
                 runtime++;
+                currentLevel++;
             }
 
             else
             {
                 switch (blink)
                 {
+                    // Red
                     case 1:
                         pictureBox_Red.BackColor = Color.Salmon;
                         break;
                     case 5:
-                        pictureBox_Red.BackColor = Color.Red;
+                        pictureBox_Red.BackColor = Color.OrangeRed;
                         break;
+                    // Green
                     case 2:
-                        pictureBox_Green.BackColor = Color.LightGreen;
+                        pictureBox_Green.BackColor = Color.GreenYellow;
                         break;
                     case 6:
-                        pictureBox_Green.BackColor = Color.Green;
+                        pictureBox_Green.BackColor = Color.YellowGreen;
                         break;
+                    // Blue
                     case 3:
-                        pictureBox_Blue.BackColor = Color.LightBlue;
+                        pictureBox_Blue.BackColor = Color.DeepSkyBlue;
                         break;
                     case 7:
-                        pictureBox_Blue.BackColor = Color.Blue;
+                        pictureBox_Blue.BackColor = Color.DodgerBlue;
                         break;
+                    // Yellow
                     case 4:
-                        pictureBox_Yellow.BackColor = Color.LightYellow;
+                        pictureBox_Yellow.BackColor = Color.Gold;
                         break;
                     case 8:
-                        pictureBox_Yellow.BackColor = Color.Yellow;
+                        pictureBox_Yellow.BackColor = Color.Orange;
                         break;
                 }
+
                 if (blink > 4)
                 {
                     placeInArray++;
                     if (placeInArray < colors.Length)
                         blink = colors[placeInArray];
                 }
+
                 else
                     blink += 4;
             }
         }
 
-        public void button_Start_Click(object sender, EventArgs e)
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            int picture = ConvertToInteger(pictureBox);
+
+            if (picture == colors[click-1])
+            {
+                countClick++;
+                click++;
+
+                if (countClick <= runtime)
+                {
+                    if (click == runtime)
+                    {
+                        click = 1;
+                        timer1.Start();
+                        CheckWinner(true); // Check if player won the game
+                        countClick = 1;
+                    }
+
+                    timer2.Stop();
+                }
+            }
+
+            else
+            {
+                timer2.Stop();
+                CheckWinner(false); // Lost the game
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            if (currentLevelStatus == false)
+                this.Text = "!!! YOU ARE IN LEVEL " + currentLevel + " / 10 !!!";
+            if (currentLevelStatus == true)
+                this.Text = "Simon by Daniel Naaman and Yonatan Shoshani";
+
+            currentLevelStatus = !currentLevelStatus;
+        }
+
+        private int ConvertToInteger(PictureBox pictureBox)
+        {
+            // Converts a string to integer following the scheme on timer1_Tick switch()
+
+            if (pictureBox.Name.EndsWith("Red"))
+            {
+                return 1;
+            }
+
+            else if (pictureBox.Name.EndsWith("Green"))
+            {
+                return 2;
+            }
+
+            else if (pictureBox.Name.EndsWith("Blue"))
+            {
+                return 3;
+            }
+
+            else // if (pictureBox.Name.EndsWith("Yellow"))
+            {
+                return 4;
+            }
+        }
+
+        private void pictureBox_Click(object sender, EventArgs e)
+        {
+            pictureBox = sender as PictureBox;
+            timer2.Start();
+        }
+
+        private void button_Start_Click(object sender, EventArgs e)
         {
             runtime = 1;
             timer1.Start();
-        }
+
+            Form1 f1 = new Form1();
+            f1.Text = "ani homo";
+        } // Check level progress notify
 
         private void button_Instructions_Click(object sender, EventArgs e)
         {
@@ -107,62 +211,6 @@ namespace Simon
         {
             Form2 f2 = new Form2();
             f2.Show();
-        }
-
-        private void pictureBox_Click(object sender, EventArgs e)
-        {
-            pictureBox = sender as PictureBox;
-
-            timer2.Start();
-
-            //if (runtime < colors.Length)
-            //{
-            //    runtime++;
-            //    timer1.Start();
-            //}
-        }
-
-        private void timer2_Tick(object sender, EventArgs e)
-        {
-            int picture = ConvertToInteger(pictureBox);
-
-            if (picture == colors[click])
-            {
-                click++;
-            }
-
-            else
-            {
-                timer2.Stop();
-                timer1.Start();
-                MessageBox.Show("You are fat and you suck"); // Change comment ^backlog
-                click = 0;
-            }
-        }
-
-    private int ConvertToInteger(PictureBox pictureBox)
-        {
-            // Converts a string to integer following the scheme on timer1_Tick switch()
-
-            if (pictureBox.Name.EndsWith("Red"))
-            {
-                return 1;
-            }
-
-            else if (pictureBox.Name.EndsWith("Yellow"))
-            {
-                return 4;
-            }
-
-            else if (pictureBox.Name.EndsWith("Green"))
-            {
-                return 2;
-            }
-
-            else // Blue
-            {
-                return 3;
-            }
         }
     }
 }
